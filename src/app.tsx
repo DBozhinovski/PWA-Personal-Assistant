a382b4f8d8095cb484ae4a8ca8a4e49db8acc875
Darko Bozhinovski <hello@darko.io>
2022-03-18 15:21:30 +0100

    Adds the rest rest code for part 1

--- src/app.tsx
+++ src/app.tsx
@@ -10,23 +10,33 @@ const initialState = {
 	messages: [],
 }
 
+// Message contents and owner, so we can differentiate those messages and style them accordingly 
 interface Message {
 	message: string
 	owner: 'me'|'bot'
 }
 
+// Our message history
 interface ReducerState {
 	messages: Message[];
 };
 
+// Our (currently) only reducer action and its payload
 interface ReducerAction {
 	type: 'addMessage';
 	payload: { message: Message }
 };
 
+// The reducer which will handle message changes
 const reducer = (state: ReducerState, action: ReducerAction) => {
 	switch (action.type) {
-		case 'addMessage': return { messages: [...state.messages, action.payload.message] }
+		case 'addMessage': {
+			return { messages: [...state.messages, action.payload.message] };
+		}
+		default: {
+			console.error("I’m sorry, Dave. I’m afraid I can’t do that.");
+			return state;
+		}
 	}
 }
 
@@ -37,14 +47,16 @@ export function App() {
 	return (
 		<div className="chat-root">
       <ReloadPrompt />
+			{/* Render message history */}
 			<ChatHistory messages={state.messages} />
+
       <ChatInput onChange={(val) => { setMessage(val) }} onSubmit={() => { 
-				dispatch({ type: 'addMessage', payload: { message: { message, owner: 'me'} } });
+				dispatch({ type: 'addMessage', payload: { message: { message, owner: 'me'} } }); // Add message to state
 				setTimeout(() => {
 					const reply = getReply(message);
 					dispatch({ type: 'addMessage', payload: { message: { message: reply, owner: 'bot'} } });
 				}, 1000);
-				setMessage('');
+				setMessage(''); // Reset message, so we can input a new one
 			}} message={message} />
 		</div>
 	);

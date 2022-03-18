01376d5f9f1154889d6daf7e726beb7da6fe6568
Darko Bozhinovski <hello@darko.io>
2022-03-18 14:59:43 +0100

    Adds the rest of the code for pt.1

--- src/app.tsx
+++ src/app.tsx
@@ -1,17 +1,51 @@
-import { useState } from 'preact/hooks';
+import { useState, useReducer } from 'preact/hooks';
 
 import ReloadPrompt from './ReloadPrompt';
 import { ChatHistory } from './ChatHistory';
 import { ChatInput } from './ChatInput';
 
+import { getReply } from './bot';
+
+const initialState = {
+	messages: [],
+}
+
+interface Message {
+	message: string
+	owner: 'me'|'bot'
+}
+
+interface ReducerState {
+	messages: Message[];
+};
+
+interface ReducerAction {
+	type: 'addMessage';
+	payload: { message: Message }
+};
+
+const reducer = (state: ReducerState, action: ReducerAction) => {
+	switch (action.type) {
+		case 'addMessage': return { messages: [...state.messages, action.payload.message] }
+	}
+}
+
 export function App() {
-  const [message, setMessage] = useState<string>('')
+  const [message, setMessage] = useState<string>('');
+	const [state, dispatch] = useReducer(reducer, initialState);
 
 	return (
 		<div className="chat-root">
       <ReloadPrompt />
-			<ChatHistory messages={[]} />
-      <ChatInput onChange={(val) => { setMessage(val) }} onSubmit={() => { setMessage(''); }} message={message} />
+			<ChatHistory messages={state.messages} />
+      <ChatInput onChange={(val) => { setMessage(val) }} onSubmit={() => { 
+				dispatch({ type: 'addMessage', payload: { message: { message, owner: 'me'} } });
+				setTimeout(() => {
+					const reply = getReply(message);
+					dispatch({ type: 'addMessage', payload: { message: { message: reply, owner: 'bot'} } });
+				}, 1000);
+				setMessage('');
+			}} message={message} />
 		</div>
 	);
 }
---
+++ src/bot/index.tsx
@@ -0,0 +1,3 @@
+export const getReply = (message: string) => {
+  return message;
+}

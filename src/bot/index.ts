f18e8f4494c613a781f204ef75eef08aea40fb6d
Darko Bozhinovski <hello@darko.io>
2022-03-25 07:40:36 +0100

    Adds bootstrap for bot

--- src/bot/index.ts
+++ src/bot/index.ts
@@ -1,3 +1,7 @@
+import { initBot } from "./bootstrap"; 
+
+initBot();
+
 export const getReply = (message: string) => {
   return message;
 }
---
+++ src/nlp.d.ts
@@ -0,0 +1,3 @@
+declare module '@nlpjs/core';
+declare module '@nlpjs/nlp';
+declare module '@nlpjs/lang-en-min';
